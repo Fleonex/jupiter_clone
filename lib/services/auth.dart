0980c58fc3ff1852906a0e8b6347ce1d9c237ab5
mortal-ghost <anmoldeepsingh148@gmail.com>
2023-04-27 12:29:13 +0530

    Added authentication backend

---
+++ lib/services/auth.dart
@@ -0,0 +1,35 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+class AuthService {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  // Sign in with Google
+  Future<User?> signInWithGoogle() async {
+    try {
+      final GoogleSignInAccount? googleSignInAccount =
+          await GoogleSignIn().signIn();
+      final GoogleSignInAuthentication googleSignInAuthentication =
+          await googleSignInAccount!.authentication;
+      final AuthCredential credential = GoogleAuthProvider.credential(
+        accessToken: googleSignInAuthentication.accessToken,
+        idToken: googleSignInAuthentication.idToken,
+      );
+      final UserCredential authResult =
+          await _auth.signInWithCredential(credential);
+      final User? user = authResult.user;
+      return user;
+    } catch (e) {
+      return null;
+    }
+  }
+
+  Future signOut() async {
+    try {
+      _auth.signOut();
+      return "success";
+    } catch (e) {
+      return "error";
+    }
+  }
+}
