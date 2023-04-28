1948705eae0c43e408879ec0adbfff6802eaedbc
M Samson <83911436+Fleonex@users.noreply.github.com>
2023-04-28 21:51:54 +0530

    Added login page

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:jupiter_clone/constants.dart';
 import 'package:jupiter_clone/firebase_options.dart';
-import 'package:jupiter_clone/screens/home.dart';
-import 'package:jupiter_clone/screens/signin_page.dart';
+import 'package:jupiter_clone/Screens/Welcome/welcome_screen.dart';
+import 'package:jupiter_clone/constants.dart';
+
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -21,11 +23,33 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Auth',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const AuthWrap(),
+          primaryColor: kPrimaryColor,
+          scaffoldBackgroundColor: Colors.white,
+          elevatedButtonTheme: ElevatedButtonThemeData(
+            style: ElevatedButton.styleFrom(
+              elevation: 0,
+              primary: kPrimaryColor,
+              shape: const StadiumBorder(),
+              maximumSize: const Size(double.infinity, 56),
+              minimumSize: const Size(double.infinity, 56),
+            ),
+          ),
+          inputDecorationTheme: const InputDecorationTheme(
+            filled: true,
+            fillColor: kPrimaryLightColor,
+            iconColor: kPrimaryColor,
+            prefixIconColor: kPrimaryColor,
+            contentPadding: EdgeInsets.symmetric(
+                horizontal: defaultPadding, vertical: defaultPadding),
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.all(Radius.circular(30)),
+              borderSide: BorderSide.none,
+            ),
+          )),
+      home: const WelcomeScreen(),
     );
   }
 }
@@ -38,13 +62,13 @@ class AuthWrap extends StatelessWidget {
     return StreamBuilder(
       stream: FirebaseAuth.instance.authStateChanges(),
       builder: (context, snapshot) {
-        if (!snapshot.hasData) {
-          // ! TODO: Add a Sign In Page
-          return const SignInPage();
-        } else {
-          // ! TODO: Add a Home Page
-          return const HomePage();
-        }
+        // if (!snapshot.hasData) {
+        //   // ! TODO: Add a Sign In Page
+        //   return const SignInPage();
+        // } else {
+        //   // ! TODO: Add a Home Page
+        //   return const HomePage();
+        // }
       },
     );
   }
