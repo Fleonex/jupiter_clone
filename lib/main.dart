8f49809680094af81b2ed800b3e14749d2925f2e
saiakshitharthi <asaiakshith03@gmail.com>
2023-04-29 14:39:11 +0530

    google sheets

--- lib/main.dart
+++ lib/main.dart
@@ -3,13 +3,12 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:jupiter_clone/constants.dart';
 import 'package:jupiter_clone/firebase_options.dart';
-import 'package:jupiter_clone/Screens/Welcome/welcome_screen.dart';
-import 'package:jupiter_clone/constants.dart';
-
+import 'package:jupiter_clone/screens/Welcome/welcome_screen.dart';
+import 'api/sheets/initial_data.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
+  await InitialData.init();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
@@ -54,22 +53,22 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class AuthWrap extends StatelessWidget {
-  const AuthWrap({Key? key}) : super(key: key);
+// class AuthWrap extends StatelessWidget {
+//   const AuthWrap({Key? key}) : super(key: key);
 
-  @override
-  Widget build(BuildContext context) {
-    return StreamBuilder(
-      stream: FirebaseAuth.instance.authStateChanges(),
-      builder: (context, snapshot) {
-        // if (!snapshot.hasData) {
-        //   // ! TODO: Add a Sign In Page
-        //   return const SignInPage();
-        // } else {
-        //   // ! TODO: Add a Home Page
-        //   return const HomePage();
-        // }
-      },
-    );
-  }
-}
+//   // @override
+//   // Widget build(BuildContext context) {
+//   //   return StreamBuilder(
+//   //     stream: FirebaseAuth.instance.authStateChanges(),
+//   //     builder: (context, snapshot) {
+//   //       if (!snapshot.hasData) {
+//   //         // ! TODO: Add a Sign In Page
+//   //         return const SignInPage();
+//   //       } else {
+//   //         // ! TODO: Add a Home Page
+//   //         return const HomePage();
+//   //       }
+//   //     },
+//   //   );
+//   // }
+// }
