d6f7c3085c7acc972ef9b77e630b06d119dc3c7d
saiakshitharthi <asaiakshith03@gmail.com>
2023-04-29 17:56:16 +0530

    random commit

---
+++ lib/services/auth.dart
@@ -0,0 +1,51 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+class AuthService {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+
+  signInWithEmailAndPassword(String email, String password) async {
+    try {
+      final UserCredential authResult = await _auth.signInWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+
+      final User? user = authResult.user;
+      return user;
+    } catch (e) {
+      return null;
+    }
+  }
+
+  // Sign in with Google
+  Future<User?> signInWithGoogle() async {
+    try {
+      final GoogleSignInAccount? googleSignInAccount =
+          await GoogleSignIn().signIn();
+      final GoogleSignInAuthentication googleSignInAuthentication =
+          await googleSignInAccount!.authentication;
+      final AuthCredential credential = GoogleAuthProvider.credential(
+        accessToken: googleSignInAuthentication.accessToken,
+        idToken: googleSignInAuthentication.idToken,
+      );
+      final UserCredential authResult =
+          await _auth.signInWithCredential(credential);
+      final User? user = authResult.user;
+      return user;
+    } catch (e) {
+      return null;
+    }
+  }
+
+  Future signOut() async {
+    try {
+      _auth.signOut();
+      return "success";
+    } catch (e) {
+      return "error";
+    }
+  }
+}
