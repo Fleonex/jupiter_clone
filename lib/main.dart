056ba2024924d15f43f5cb5b9f92fe017d9284d7
M Samson <83911436+Fleonex@users.noreply.github.com>
2023-04-30 15:24:55 +0530

    Added profile.dart

--- lib/main.dart
+++ lib/main.dart
@@ -48,7 +48,7 @@ class MyApp extends StatelessWidget {
               borderSide: BorderSide.none,
             ),
           )),
-      home: Dashboard(),
+      home: ProfilePage(),
     );
   }
 }
